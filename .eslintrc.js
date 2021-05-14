50c38884439c674de5fa902aaeb9e27ab59cc6e6
xi <xizhou93@gmail.com>
2021-05-14 16:10:38 +0200

    Set up environment

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    'comma-dangle': [2, 'never'],
+    semi: [2, 'always']
+  }
+};
