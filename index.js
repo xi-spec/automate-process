7dfe4d226e7a557e05bed887a3aeb936dc7a5104
xi <xizhou93@gmail.com>
2021-05-14 16:59:03 +0200

    Add simplify  gencatAutomateProcess

--- index.js
+++ index.js
@@ -6,15 +6,10 @@ const SEARCH_WORD = 'agenda cultural';
 const SEARCH_INPUT = '#cercadorOcultGoogle';
 const SEARCH_ICON = '[aria-label="Cercar"]';
 
-async function gencatAutomateProcess () {
+(async function gencatAutomateProcess () {
   const browser = await chromium.launch({ headless: false, slowMo: 50 });
   const page = await browser.newPage();
   await page.goto(GENCAT_URL);
-  await page.click(SEARCH_INPUT);
-  await page.type(SEARCH_INPUT, SEARCH_WORD);
+  await page.fill(SEARCH_INPUT, SEARCH_WORD);
   await page.click(SEARCH_ICON);
-};
-
-gencatAutomateProcess();
-
-module.exports = gencatAutomateProcess;
+})();
